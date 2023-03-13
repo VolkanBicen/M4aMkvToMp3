a5e972f0548a5a208dd4b1c5845532224befc311
Volkan BİÇEN <vbicen1@gmail.com>
2023-03-13 16:58:37 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using NAudio.Wave;
+
+Console.WriteLine("Input Folder Path");
+string? inputFolderPath = Console.ReadLine();
+
+Console.WriteLine("Output Folder Path");
+string? outputFolderPath = Console.ReadLine();
+
+if (!Directory.Exists(inputFolderPath))
+{
+    Console.WriteLine("Please specify a valid input folder!");
+    return;
+}
+
+if (!Directory.Exists(outputFolderPath))
+{
+    Console.WriteLine("Please specify a valid output folder!");
+    return;
+}
+
+var inputFiles = Directory.GetFiles(inputFolderPath, "*.m4a", SearchOption.AllDirectories)
+        .Concat(Directory.GetFiles(inputFolderPath, "*.mkv", SearchOption.AllDirectories))
+        .ToArray();
+
+if (inputFiles.Length == 0)
+{
+    Console.WriteLine(".Mkv or .M4a file found in folder!");
+    return;
+}
+
+Console.WriteLine($"A total of  {inputFiles.Length} valid files were found.");
+
+int count = 0;
+foreach (var inputFile in inputFiles)
+{
+    var fileName = Path.GetFileNameWithoutExtension(inputFile);
+    var outputPath = Path.Combine(outputFolderPath, fileName + ".mp3");
+
+    using (var reader = new MediaFoundationReader(inputFile))
+    {
+        MediaFoundationEncoder.EncodeToMp3(reader, outputPath);
+    }
+
+    // Rename the file
+    // File.Move(outputPath, Path.Combine(outputFolderPath , fileName + "-volkan" + ".mp3"));
+
+    count++;
+    Console.WriteLine($"File {inputFile} Converted. ({count}/{inputFiles.Length})");
+}
+
+Console.WriteLine($"{count} files converted.");
+
+Console.ReadKey();
---
+++ README.md
@@ -0,0 +1,2 @@
+# M4aMkvToMp3
+ searches for both .m4a and .mkv files in the input folder and converts each file to MP3 format
